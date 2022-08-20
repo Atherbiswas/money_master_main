d87d62112efcf76791dbd9c0861a3d41eec0f6e1
Ather1230 <ather.biswas0057@gmail.com>
2022-08-20 12:59:00 +0600

    added

--- script/utilities.js
+++ script/utilities.js
@@ -1,3 +1,4 @@
+//common function for input field...
 function getInputFieldValueById (inputId){
     const inputField = document.getElementById(inputId);
     const inputFieldAmountString = inputField.value;
@@ -5,12 +6,16 @@ function getInputFieldValueById (inputId){
     inputField.value = '';
     return inputFieldAmount;
 }
+
+//common function for text field...
 function getTextElementValueById(elementId){
     const textElement = document.getElementById(elementId);
     const textElementAmountString = textElement.innerText;
     const textElementAmount = parseFloat(textElementAmountString);
     return textElementAmount;
 }
+
+//common function for set value...
 function setTextElementValueById(elementId,newValue){
     const textElement = document.getElementById(elementId);
     textElement.innerText = newValue;
